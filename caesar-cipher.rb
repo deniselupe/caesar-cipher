d1684aa6bb910de3609b969708948bc938bccb09
deniselupe <dgrodriguez24@hotmail.com>
2021-12-01 21:33:39 +0000

    feat: support shift value outside of range -26..26

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,8 +1,16 @@
 #Assuming that the argument for parameter is either a positive or negative integer
-#Assuming that the argument for paremeter is a number between -26 and 26
 #Assuming that string argument contains only letters in the alphabet and no special characters or numbers
 
 def ceasar_cipher(string, shift)
+  if shift.abs > 26
+    if shift < 0
+      shift = shift.abs % 26
+      shift = shift * -1
+    else
+      shift = shift.abs % 26
+    end
+  end
+  
   new_string = string.split('')
   
   new_string = new_string.map do |letter|
@@ -26,10 +34,10 @@ def ceasar_cipher(string, shift)
       char_number += shift
     end
   end
-
+  
   new_string = new_string.map { |char| char.chr }
   new_string = new_string.join('')
   puts new_string
 end
 
-ceasar_cipher('cat', 15)
+ceasar_cipher('cat', -55)
